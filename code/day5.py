d29c0586fd34fee846a356f30c8a246d282d2284
Alex <armzimmer@web.de>
2025-01-07 13:00:17 +0100

    improve readability of day 5 solution, and add git ignore

--- code/day5.py
+++ code/day5.py
@@ -4,30 +4,29 @@ input_file = "/home/alex/Nextcloud (Kopie)/Documents_old/interestingtoread/adven
 day5_input.txt"
 
 def main():
+    with open(input_file, 'r') as file:
+        jumps = []
+        for line in file.readlines():
+            jumps.append(int(line))
 
-    file = open(input_file, 'r')
-    jumps = []
-    for line in file.readlines():
-        jumps.append(int(line))
+        print("file:", jumps)
 
-    print("file:", jumps)
+        pose = 0
+        cntr = 0
 
-    pose = 0
-    cntr = 0
-
-    # update pose, value+=1
-    while pose >= 0 and pose < len(jumps):
-        if jumps[pose] < 3: # and jumps[pose] > -3
-            # part 1 only this if case
-            new_pose = pose + jumps[pose]
-            jumps[pose] += 1
-        else:
-            new_pose = pose + jumps[pose]
-            jumps[pose] -= 1
-        pose = new_pose
-        cntr += 1
-    
-    print("after", cntr, "jumps we got out!")
+        # update pose, value+=1
+        while pose >= 0 and pose < len(jumps):
+            if jumps[pose] < 3: # and jumps[pose] > -3
+                # part 1: only use the contents of this if-case
+                new_pose = pose + jumps[pose]
+                jumps[pose] += 1
+            else:
+                new_pose = pose + jumps[pose]
+                jumps[pose] -= 1
+            pose = new_pose
+            cntr += 1
+        
+        print("after", cntr, "jumps we got out!")
 
 
 
